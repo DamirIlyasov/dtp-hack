b9a6bbcfce1f4676d06484fa1700cefdcea524c8
DamirIlyasov <damirilyasov1997@gmail.com>
2017-10-25 01:14:41 +0300

    Methods in Main controller defined for swagger

--- src/main/java/com/hackteam/dtp/controller/MainController.java
+++ src/main/java/com/hackteam/dtp/controller/MainController.java
@@ -2,18 +2,19 @@ package com.hackteam.dtp.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
     @ResponseBody
-    @RequestMapping("/")
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String getHelloMessage() {
         return "Server online.";
     }
 
     @ResponseBody
-    @RequestMapping("/secure")
+    @RequestMapping(value = "/secure", method = RequestMethod.GET)
     public String testSecure() {
         return "Your token is correct! KRASAVA";
     }
