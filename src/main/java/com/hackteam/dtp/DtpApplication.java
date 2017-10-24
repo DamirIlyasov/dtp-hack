c51603d60f2d164482f2e27a3e623ec137ab1eb0
DamirIlyasov <damirilyasov1997@gmail.com>
2017-10-24 14:59:32 +0300

    initial commit

---
+++ src/main/java/com/hackteam/dtp/DtpApplication.java
@@ -0,0 +1,12 @@
+package com.hackteam.dtp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DtpApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hackteam/dtp/DtpApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hackteam.dtp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DtpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
