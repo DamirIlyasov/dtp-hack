8e1538780fda519e4501b5e09d12b1a97d95afb8
DamirIlyasov <damirilyasov1997@gmail.com>
2017-10-25 01:12:31 +0300

    swagger config improved

--- src/main/java/com/hackteam/dtp/config/SwaggerConfig.java
+++ src/main/java/com/hackteam/dtp/config/SwaggerConfig.java
@@ -17,7 +17,7 @@ public class SwaggerConfig {
     public Docket newsApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.hackteam.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.hackteam.dtp.controller"))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo());
